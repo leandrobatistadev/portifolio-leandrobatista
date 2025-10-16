3919d810b1e9cc2a1ea2e506830fb71fadec24a9
leandrobatistadev <@leandrobatista.dev@gmail.com>
2025-10-16 07:15:42 -0300

    Primeiro commit do portfólio

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfólio Leandro Batista
---
+++ javascript.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("#form-contato");
+  const telefone = "5581986997597";
+
+  form.addEventListener("submit", function (evento) {
+    evento.preventDefault(); // impede o envio padrão do form
+
+    const nome = document.querySelector("#nome").value.trim();
+    const mensagem = document.querySelector("#mensagem").value.trim();
+
+    // Evita abrir o WhatsApp sem dados
+    if (!nome || !mensagem) {
+      alert("Por favor, preencha seu nome e sua mensagem antes de enviar.");
+      return;
+    }
+
+    const texto = `Olá, me chamo ${nome}. ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, "_blank");
+  });
